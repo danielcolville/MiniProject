236c6081102c577ea93638bc7677dafee1782c35
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-09-03 00:06:19 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,44 @@
+package session3;
+//class Base {
+	
+//}
+//public class Test {
+	class Base {
+		public static void main(String[] args){
+			String str = "StudyTonight"; 
+			str.concat(".com"); str = str.toUpperCase(); str.replace("TONIGHT","today"); 
+			System.out.println(str);
+		}
+	}
+	public class Test extends Base {
+		
+	}
+	/*
+	class Derived extends Base {
+		String s="Derived";
+		
+	}
+	
+	public class Test {
+		void print(Base b) {
+			System.out.println(b.show());
+		}
+		void print(Derived d) {
+			System.out.println(d.show());
+		}
+		public static void main(String[] args){
+			int a=5;
+			Test t=new Test();
+			Base b=new Derived();
+			t.print(b);
+			//System.out.println("Hello");
+		}
+	}*/
+	//public static void main(String[] args) {
+		//class Base{
+		//}// }
+		 //public class Test extends Base{}
+//}
+	//}
+
+//}
